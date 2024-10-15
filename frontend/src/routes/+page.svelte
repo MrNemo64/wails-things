9f84fadf48760ddf53d8fcfdf2b9b7d0ed7e08f5
MrNemo64 <58314381+MrNemo64@users.noreply.github.com>
2024-10-15 19:01:13 +0200

    init

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { ClipboardGetText } from '$lib/wailsjs/runtime/runtime';
+
+	function getWithClipboardGetText() {
+		ClipboardGetText().then(console.log).catch(console.error);
+	}
+
+	async function getWithJs() {
+		if (!navigator.clipboard) throw new Error('No clipboard');
+		const items = await navigator.clipboard.read();
+		items.forEach((item) => {
+			item.presentationStyle;
+		});
+	}
+</script>
+
+<button class="btn btn-neutral" on:click={getWithClipboardGetText}>
+	get clipboard with ClipboardGetText
+</button>
+<button class="btn btn-neutral" on:click={getWithJs}> get clipboard with JS </button>
